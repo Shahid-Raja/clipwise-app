4492c2f3816a5878acb6b951cd69df1a6bff1b89
Shahid Raja <Shahid.Raja@ainetix.com>
2024-09-30 00:43:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HeaderComponent from './components/header/header'
+
+function App() {
+ 
+
+  return (
+    <>
+      <HeaderComponent/>
+    </>
+  )
+}
+
+export default App
